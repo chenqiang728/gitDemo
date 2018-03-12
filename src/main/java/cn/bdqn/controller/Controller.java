84f47f7ae99b312ba17aed5907f55a83bd7c6a2d
chenqiang728 <358698895@qq.com>
2018-03-12 14:10:16 +0800

    测试

---
+++ src/main/java/cn/bdqn/controller/Controller.java
@@ -0,0 +1,12 @@
+package cn.bdqn.controller;
+
+/**
+ * Created by Administrator on 2018/3/12 0012.
+ */
+public class Controller {
+    public static void main(String[] args) {
+        Integer a =9;
+        Integer b =10;
+        System.out.println(a+b);
+    }
+}
